b8caed59a5aa804d8532e15e37f4173373adcbd1
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-11-09 22:39:20 +0300

    Test for Optional Chaining

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ const person = {
 document.getElementById('text1').innerHTML = person.name;
 document.getElementById('text2').innerHTML = person.surname;
 document.getElementById('text3').innerHTML = person.age;
+
+//Test for Optional Chaining
+const person3 = {
+    name: 'Ulugbek',
+    age: 25
+    };
+    const companyName = person3.job?.companyName;
+    console.log(companyName);
+    console.log(person3.study?.());
+    console.log(person3.job);
